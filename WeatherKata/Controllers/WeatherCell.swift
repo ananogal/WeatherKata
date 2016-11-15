080535e14e8b92044c855a1d8bd88705d8174e11
Ana Nogal <ana.nogal@crowdmix.me>
2016-11-15 14:13:13 +0000

    Added images and design

---
+++ WeatherKata/Controllers/WeatherCell.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCell.swift
+//  WeatherKata
+//
+//  Created by Ana Nogal on 15/11/2016.
+//  Copyright © 2016 Ana Nogal. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var rainChanceLabel: UILabel!
+    @IBOutlet weak var weatherImg: UIImageView!
+}
