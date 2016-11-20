2dba4e65667189b7f81795113aecb48662cc97b5
Ana Nogal <ana.nogal@crowdmix.me>
2016-11-20 00:01:43 +0000

    Set receiver on interactor

---
+++ WeatherKata/Services/WeatherInteractorType.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherInteractorType.swift
+//  WeatherKata
+//
+//  Created by Ana Nogal on 19/11/2016.
+//  Copyright © 2016 Ana Nogal. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherKataTests/Mocks/WeatherInteractorSpy.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherInteractorSpy.swift
+//  WeatherKata
+//
+//  Created by Ana Nogal on 19/11/2016.
+//  Copyright © 2016 Ana Nogal. All rights reserved.
+//
+
+import Foundation
